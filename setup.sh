56969a979f41c5eefc38389bcd7dfabbdf8f01eb
hex-plex <sendhilsom7@gmail.com>
2021-08-26 01:31:01 +0530

    Added aruco

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 cp -r dot_gazebo/world/ps1/my_ground_plane ~/.gazebo/models/my_ground_plane
 cp -r dot_gazebo/world/ps2/ps2_ground_plane ~/.gazebo/models/ps2_ground_plane
+cp -r dot_gazebo/world/ps1/Apriltag36/* ~/.gazebo/models/
+
 echo "Done Copying models"
