28aa793b1bc93e05169a1a1949e7bb06811e5ee7
pranav2oo1 <2001pranavmittal@gmail.com>
2021-09-16 09:46:44 +0530

    Added Odom : Needs Tuning

---
+++ dot_control/src/omnidrive_node.cpp
@@ -0,0 +1,20 @@
+#include "dot_control/omni_control.hpp"
+#include <ros/ros.h>
+// Equations are taken from this repo -> https://github.com/GuiRitter/OpenBase
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "omnidrive");
+    ros::NodeHandle n;
+    omnidrive::drive controller(n);
+    ros::Rate rate(10);
+    while(ros::ok()){
+        //std::cout << "SENDING DATA\n";
+        //ROS_INFO_STREAM_THROTTLE(2.0,"Sending Data ROS INFO");
+        controller.publishVel();
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
